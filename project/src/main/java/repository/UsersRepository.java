c74375664b719f5ba14fd6be78d2407de43bd5eb
rmsp7223123 <129022648+rmsp7223123@users.noreply.github.com>
2024-01-07 23:10:51 +0900

    header, body templates

--- project/src/main/java/repository/UsersRepository.java
+++ project/src/main/java/repository/UsersRepository.java
@@ -9,6 +9,9 @@ import entity.Users;
 
 @Repository
 public interface UsersRepository extends JpaRepository<Users, Long> {
+	
+	
+	
 	Optional<Users> findByLoginId(String loginId);
 
 	Boolean existsByLoginId(String loginId);
