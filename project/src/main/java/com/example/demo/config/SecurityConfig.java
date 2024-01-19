176e005aa24db5c7eb6dcf87512de499a0a5af07
rmsp7223123 <129022648+rmsp7223123@users.noreply.github.com>
2024-01-19 22:35:34 +0900

    +

--- project/src/main/java/com/example/demo/config/SecurityConfig.java
+++ project/src/main/java/com/example/demo/config/SecurityConfig.java
@@ -3,14 +3,14 @@ package com.example.demo.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+import com.example.demo.handler.LoginSuccessHandler;
+import com.example.demo.handler.LogoutSuccessHandler;
 import com.example.demo.service.UserService;
 
 import lombok.RequiredArgsConstructor;
@@ -29,13 +29,18 @@ public class SecurityConfig {
 	private BCryptPasswordEncoder passwordEncoder;
 
 	@Bean
-	public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
-		return configuration.getAuthenticationManager();
-	}
-
-	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
+		http.authenticationProvider(authenticationProvider())
+				.csrf().disable().cors().and().authorizeRequests()
+				.requestMatchers("/users/login", "/login", "/users/register", "/users/signup", "/", "css/**",
+						"datatables/**", "demo/**", "img/**", "jquery/**", "js/demo/**", "js/**", "mixins/**",
+						"navs/**", "scss/**", "scss/mixins/**", "scss/utilities/**", "scss/vendor/**", "utilities/**",
+						"vendor/**")
+				.permitAll().anyRequest().authenticated().and().formLogin().loginPage("/").usernameParameter("loginId")
+				.passwordParameter("password").successHandler(new LoginSuccessHandler(userRepository)).and().logout()
+				.logoutUrl("/users/logout").invalidateHttpSession(true).deleteCookies("JSESSIONID")
+				.logoutSuccessHandler(new LogoutSuccessHandler());
 		return http.build();
 	}
 
