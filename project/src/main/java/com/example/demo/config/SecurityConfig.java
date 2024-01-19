d80570e277ab2007bc978902c5a2520d1097aa9c
rmsp7223123 <129022648+rmsp7223123@users.noreply.github.com>
2024-01-19 19:01:35 +0900

    ++

--- project/src/main/java/com/example/demo/config/SecurityConfig.java
+++ project/src/main/java/com/example/demo/config/SecurityConfig.java
@@ -4,37 +4,23 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.SecurityBuilder;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import com.example.demo.handler.LoginFailureHandler;
-import com.example.demo.handler.LoginSuccessHandler;
-import com.example.demo.handler.LogoutSuccessHandler;
-import com.example.demo.jwt.LoginFilter;
 import com.example.demo.service.UserService;
 
 import lombok.RequiredArgsConstructor;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
 
-//	private final com.example.demo.repository.UsersRepository userRepository;
-
-	private final AuthenticationConfiguration authenticationConfiguration;
-
-	public SecurityConfig(AuthenticationConfiguration authenticationConfiguration) {
-		this.authenticationConfiguration = authenticationConfiguration;
-	}
+	private final com.example.demo.repository.UsersRepository userRepository;
 
 	@Autowired
 	private UserService userService;
@@ -49,14 +35,6 @@ public class SecurityConfig {
 
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-
-		http.csrf((auth) -> auth.disable());
-		http.formLogin((auth) -> auth.disable());
-		http.httpBasic((auth) -> auth.disable());
-		http.authorizeHttpRequests((auth) -> auth.requestMatchers("/").permitAll().anyRequest().authenticated());
-
-		http.addFilterAt(new LoginFilter(authenticationManager(authenticationConfiguration)),
-				UsernamePasswordAuthenticationFilter.class);
 
 		return http.build();
 	}
